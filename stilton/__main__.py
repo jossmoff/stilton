6e33a9b0079f18227a9504aafd3ede2edb47fb04
Joss <jossmoff@hotmail.co.uk>
2020-01-13 00:42:32 +0000

    Added method stubs for CLI tool

---
+++ stilton/__main__.py
@@ -0,0 +1,11 @@
+from argparse import ArgumentParser
+
+MODULE_NAME = "stilton: The command line tool for code smell refactoring."
+__version__ = "0.0.1"
+
+
+def main():
+    pass
+                        
+if __name__ == "__main__":
+  main()
