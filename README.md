67643e7fc736a7ed5fe3570e0b035701dab28671
Joss <jossmoff@hotmail.co.uk>
2020-01-15 21:28:32 +0000

    Updated README and added gitignore
    - Added badges using inline html to the README.
    - .gitignore added to ignore launch settings in vscode.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-![Stilton Banner](./logo/logo-banner.png)
+<p align=center>
+  <img src="./logo/logo-banner.png"/>
+  <a target="_blank" href="https://www.python.org/downloads/" title="Python version"><img src="https://img.shields.io/badge/python-%3E=_3.8-yellow.svg"></a>
+  <a target="_blank" href="LICENSE" title="License: MIT"><img src="https://img.shields.io/badge/License-MIT-yellow.svg"></a>
+  <img alt="GitHub release (latest by date)" src="https://img.shields.io/github/v/release/jossmoff/stilton?color=yellow">
+</p>
--- stilton/__main__.py
+++ stilton/__main__.py
@@ -1,11 +1,17 @@
 from argparse import ArgumentParser
 
-MODULE_NAME = "stilton: The command line tool for code smell refactoring."
+MODULE_NAME = "stilton: The command line tool that detects code smells for refactoring"
 __version__ = "0.0.1"
 
 
 def main():
-    pass
+    arg_parser = ArgumentParser(description=MODULE_NAME)
+    try:
+        args = arg_parser.parse_args()
+    except Exception as e:
+        traceback.print_exc()
+        exit(1)
+    exit(0)
                         
 if __name__ == "__main__":
   main()
